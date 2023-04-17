6e09f89587dc75d09878184567cfd91d4cef336c
shreyhb <116701113+shreyhb@users.noreply.github.com>
2023-04-17 16:24:33 +0530

    Add files via upload

---
+++ steps.sh
@@ -0,0 +1,11 @@
+sudo apt update && upgrade
+sudo apt install nodejs
+sudo apt install npm
+npm install express 
+npm install cors
+npm install http
+npm install ws
+npm install child_process
+npm install nodemon --save-dev
+npm install mongodb
+npm install mongoose
